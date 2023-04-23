1b75737324c577575afa49d4160bea7e7e2be4e1
JuanPabloAmador <53062595+JuanPabloAmador@users.noreply.github.com>
2023-04-23 21:01:38 +0200

    5 files modified, 1 added

--- README.md
+++
@@ -1,3 +0,0 @@
-# Curso-Lit
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-a2hggq)
---
+++ components/hello-world.js
@@ -0,0 +1,51 @@
+import { LitElement, html, css } from 'lit';
+
+class HelloWorld extends LitElement {
+  static styles = css`
+    :host {
+      display: block;
+      border: 1px solid red;
+      padding: 10px;
+      background-color: blue;
+    }
+    h1 {
+      color: white;
+    }
+    p {
+      color: white;
+    }
+    span {
+      color: white;
+    }
+  `;
+
+  static properties = {
+    title: { type: String },
+    description: { type: String },
+    counter: { type: Number },
+  };
+
+  constructor() {
+    super();
+    // this.title = 'Hello world';
+    // this.description = 'Esto es un parrafo';
+    // this.counter = 0;
+  }
+
+  render() {
+    return html`
+      <h1>${this.title}</h1>
+      <p>${this.description}</p>
+      <span>${this.counter}</span>
+      <button @click=${this.addOneToCounter}>+</button>
+    `;
+  }
+
+  addOneToCounter() {
+    console.log('counter was call');
+    this.counter++;
+    console.log(this.counter);
+  }
+}
+
+customElements.define('hello-world', HelloWorld);
